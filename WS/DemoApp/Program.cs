bf59e6c80e17e2120f1874dab2565c3721e0e826
AYB-cmd <aeljelladi@gmail.com>
2021-06-03 02:42:47 +0100

    WebServices

---
+++ WS/DemoApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebService.WebServiceSoapClient();
+
+        }
+    }
+}
